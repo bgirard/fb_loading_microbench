170d8a211d4a38c154051d6721f24ce1ece96e68
Benoit Girard <bgirard@fb.com>
2018-02-14 14:57:15 -0800

    Add proper npm install check

--- run.sh
+++ run.sh
@@ -2,5 +2,10 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if ! which npm >/dev/null; then
+  echo "npm not installed: See https://www.npmjs.com/get-npm"
+  exit 1
+fi
+
 npm install express
 node server.js
